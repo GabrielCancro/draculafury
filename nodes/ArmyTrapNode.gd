9e4046251b2ab41a22995257d7741d7d64964533
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2024-11-22 22:56:13 -0300

    update

--- nodes/ArmyTrapNode.gd
+++ nodes/ArmyTrapNode.gd
@@ -6,6 +6,15 @@ var xpos = 1
 func _ready():
 	ArmyManager.ArmyTrapNode = self
 	yield(get_tree().create_timer(.5),"timeout")
+	set_trap()
+
+func set_trap():
+	var arr = []
+	randomize()
+	for i in range(EnemyManager.max_x_pos-2): arr.append(i+1)
+	arr.shuffle()
+	arr.append(0)
+	arr.append(EnemyManager.max_x_pos-1)
 	update_trap()
 
 func update_trap():
